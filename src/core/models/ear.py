a8aae2904a2c6bf729719bd78962de9eb4bfe8af
Kieran Gibb <kg@magmacollective.org>
2026-01-22 16:44:39 +0000

    add back in shift-invariance via hard cross-decoding

--- src/core/models/ear.py
+++ src/core/models/ear.py
@@ -73,7 +73,6 @@ class VectorQuantiserEMA(torch.nn.Module):
 
     @torch.no_grad()
     def update(self, z_e: torch.Tensor, encodings: torch.Tensor) -> None:
-        """update the codebook via an exponential moving average of encoded values"""""
         # update count of encoder hidden states using a running average
         self.ema_cluster_size.data = self.ema_cluster_size.data * self.gamma + (1 - self.gamma) * encodings.sum(dim=0)
         # apply laplace smoothing
@@ -84,11 +83,8 @@ class VectorQuantiserEMA(torch.nn.Module):
         # update the codebook
         self.embedding.weight.data = self.ema_embedding.weight / self.ema_cluster_size.unsqueeze(0)
 
+    @torch.no_grad()
     def perplexity(self, encodings: Tensor) -> Tensor:
-        """
-        p = b*t*f: a unique code word for each encoding (feature magnitude)
-        p = 1: the same code word for each embedding (feature magnitude)
-        """
         avg_probs = encodings.mean(dim=0)
         return (-(avg_probs * (avg_probs + 1e-10).log()).sum()).exp()
 
@@ -122,6 +118,11 @@ class EAR(L.LightningModule):
     sigma_z: float = 1.0
     beta: float = 0.25
     gamma: float = 0.999
+    delta_sigma_min: float | None = None
+    delta_sigma_max: float = 2.0
+    delta_sigma_step_start: int | None = None
+    delta_sigma_step_end: int | None = None
+    delta_sigma_step_slope: float = 1.0
     learning_rate: float = 4e-5
     optimiser_cls: str = "torch.optim.AdamW"
     optimiser_config: DictConfig | None = None
@@ -180,16 +181,16 @@ class EAR(L.LightningModule):
             activation_fn=Activation[self.mlp_activation],
             dropout_prob=self.mlp_dropout_prob,
         )
-        # self.offset_encoder = init_alignment_encoder(
-        #     in_channels=self.cnn_block_sizes[-1] * self.cnn_block_width,
-        #     out_channels=self.cnn_block_sizes[-1] * self.cnn_block_width // 4,
-        #     in_features=self.cnn_block_sizes[-1] * self.cnn_block_width // 4 * self.latent_window_length,
-        #     cnn_kernel_size=(1, self.latent_frequency_dim),
-        #     mlp_reduction_factor=2,
-        #     flatten_start_dim=1,
-        #     activation_fn=Activation[self.mlp_activation],
-        #     out_features=1,
-        # )
+        self.offset_encoder = init_alignment_encoder(
+            in_channels=self.cnn_block_sizes[-1] * self.cnn_block_width,
+            out_channels=self.cnn_block_sizes[-1] * self.cnn_block_width // 4,
+            in_features=self.cnn_block_sizes[-1] * self.cnn_block_width // 4 * self.latent_window_length,
+            cnn_kernel_size=(1, self.latent_frequency_dim),
+            mlp_reduction_factor=2,
+            flatten_start_dim=1,
+            activation_fn=Activation[self.mlp_activation],
+            out_features=1,
+        )
         self.quantise = VectorQuantiserEMA(num_clusters=512, num_features=8, gamma=self.gamma)
         self._reset_cache()
 
@@ -242,24 +243,54 @@ class EAR(L.LightningModule):
             mel_break_frequency=self.mel_break_frequency,
         )
 
+    @property
+    def delta_sigma_params(self):
+        return dict(
+            x_min=self.delta_sigma_step_start,
+            x_max=self.delta_sigma_step_end,
+            y_min=self.delta_sigma_min,
+            y_max=self.delta_sigma_max,
+            k=self.delta_sigma_step_slope or 1.0,
+        )
+
     def forward(self, x: Tensor) -> Dict[str, Tensor]:
-        x = T.center_crop(x, [(x.size(-2) - (x.size(-2) % self.frame_window_length)), self.num_mel_bins]).float()
+        # compute shifted versions
+        x_i = T.center_crop(x, [(x.size(-2) - (x.size(-2) % self.frame_window_length)), self.num_mel_bins]).float()
+        x_i_framed = frame(x_i, window_length=self.frame_window_length, hop_length=self.frame_hop_length).flatten(end_dim=1)
+        delta = torch.randn(x_i_framed.size(0), 1, 1, 1).to(x_i.device) * self.delta_sigma_current(self.trainer.global_step)
+        x_j = translation(x_i_framed, delta, padding_mode="circular")
         # embed features
-        z_e = self.encode(x) # (bs, seq, ld)
-        # break up latent feature vector into quantisable chunks
-        z_e_chunk = torch.stack(z_e.chunk(self.latent_dim // self.quantise.num_features, dim=-1), dim=-2) # (bs, seq, ld/ck, ck)
-        # quantise using the codebook
-        z_q_chunk, encoding_idx, encodings = self.quantise(z_e_chunk) # (bs * seq * ld/ck, ck)
-        z_q = z_q_chunk.reshape(*z_e_chunk.shape[:-2], -1) # (bs, seq, ld)
-        # copy gradients
-        z_q = z_e + (z_q - z_e).detach()
-        # reconstruct
-        x_hat = self.decode(z_q)
+        z_e_i, delta_hat_i = self.encode(x_i) # (bs, seq, ld), (bs, seq, 1)
+        z_e_j, delta_hat_j = self.encode(x_j) # (bs, seq, ld), (bs, seq, 1)
+        # break up latent feature vector into quantisable chunks and quantise
+        z_e_i_chunk = torch.stack(z_e_i.chunk(self.latent_dim // self.quantise.num_features, dim=-1), dim=-2) # (bs, seq, ld/ck, ck)
+        z_q_i_chunk, encoding_idx_i, encodings_i = self.quantise(z_e_i_chunk) # (bs * seq * ld/ck, ck)
+        z_q_i = z_q_i_chunk.reshape(*z_e_i_chunk.shape[:-2], -1) # (bs, seq, ld)
+        z_q_i = z_e_i + (z_q_i - z_e_i).detach()
+        z_e_j_chunk = torch.stack(z_e_j.chunk(self.latent_dim // self.quantise.num_features, dim=-1), dim=-2) # (bs, seq, ld/ck, ck)
+        z_q_j_chunk, encoding_jdx_j, encodings_j = self.quantise(z_e_j_chunk) # (bs * seq * ld/ck, ck)
+        z_q_j = z_q_j_chunk.reshape(*z_e_j_chunk.shape[:-2], -1) # (bs, seq, ld)
+        z_q_j = z_e_j + (z_q_j - z_e_j).detach()
+        # cross-decoded reconstructions, reconstruct x_i from delta_j and x_j from delta_i
+        U_i = self.mlp_decode(z_q_i)
+        U_j = self.mlp_decode(z_q_j)
+        x_hat_i = self.cnn_decode(U_j, delta_hat_i) # (bs, 1, fr * seq, fq)
+        x_hat_i_framed = frame(x_hat_i, window_length=self.frame_window_length, hop_length=self.frame_hop_length).flatten(end_dim=1) # (bs * seq, 1, fr, fq)
+        x_hat_j = self.cnn_decode(U_i, delta_hat_j) # (bs * seq, 1, fr, fq)
         # update codebook EMA
-        self.quantise.update(z_e_chunk, encodings)
+        self.quantise.update(z_e_i_chunk, encodings_i)
+        self.quantise.update(z_e_j_chunk, encodings_j)
         # quantify diversity of codebook usage
-        perplexity = self.quantise.perplexity(encodings)
-        return dict(x=x, x_hat=x_hat, z_q=z_q, z_e=z_e, z_e_chunk=z_e_chunk, perplexity=perplexity)
+        perplexity_i = self.quantise.perplexity(encodings_i)
+        perplexity_j = self.quantise.perplexity(encodings_j)
+        return dict(
+            x_i=x_i, x_j=x_j, x_i_framed=x_i_framed,
+            x_hat_i=x_hat_i, x_hat_j=x_hat_j, x_hat_i_framed=x_hat_i_framed,
+            z_q_i=z_q_i, z_e_i=z_e_i, z_e_i_chunk=z_e_i_chunk,
+            z_q_j=z_q_j, z_e_j=z_e_j, z_e_j_chunk=z_e_j_chunk,
+            perplexity_i=perplexity_i,
+            perplexity_j=perplexity_j,
+        )
 
     def step(self, *args: Any, **kwargs: Any) -> Dict[str, Tensor]:
         return self.forward(*args, **kwargs)
@@ -267,8 +298,7 @@ class EAR(L.LightningModule):
     def encode(self, x: Tensor) -> Tensor:
         x = self.cnn_encode(x) # (bs, ch, ts, fq)
         x = self.frame_encode(x) # (bs, seq, ch, ts, fq)
-        x = self.mlp_encode(x) # (bs, seq, ld)
-        return x
+        return self.mlp_encode(x) # (bs, seq, ld), (bs, seq, 1)
 
     def cnn_encode(self, x: Tensor) -> Tensor:
         for i, block in enumerate(self.feature_encoder):
@@ -286,8 +316,10 @@ class EAR(L.LightningModule):
         bs, seq, *_ = x.size()
         x = x.flatten(end_dim=1)
         x = self.content_encoder(x)
+        delta_hat = self.offset_encoder(x)
         x = x.unflatten(dim=0, sizes=(bs, seq))
-        return x
+        delta_hat = delta_hat.unflatten(dim=0, sizes=(bs, seq))
+        return x, delta_hat
 
     def decode(self, x: Tensor) -> Tensor:
         x = self.mlp_decode(x) # (bs, seq, ch, ts, fq)
@@ -301,40 +333,62 @@ class EAR(L.LightningModule):
         x = x.unflatten(dim=0, sizes=(bs, seq))
         return x
 
-    def cnn_decode(self, x: Tensor) -> Tensor:
-        bs, seq, *_ = x.size()
-        x = x.flatten(end_dim=1)
+    def cnn_decode(self, x: Tensor, delta: Tensor) -> Tensor:
         for i, block in enumerate(self.feature_decoder):
-            if seq > 1 and i == len(self.feature_decoder) - 1:
-                num_timesteps = (self.frame_window_length * seq) // 2**(len(self.feature_decoder) - i)
-                x = unframe(x.view(bs, seq, *x.size()[1:]), hop_length=x.size(-2), num_timesteps=num_timesteps)
+            if i == len(self.feature_decoder) - 2:
+                x = translation(x, delta.view(delta.size(0) * delta.size(1), 1, 1, 1), padding_mode="circular")
+            if i == len(self.feature_decoder) - 1:
+                num_timesteps = (self.frame_window_length * delta.size(1)) // 2**(len(self.feature_decoder) - i)
+                x = unframe(x.view(delta.size(0), delta.size(1), *x.size()[1:]), hop_length=x.size(-2), num_timesteps=num_timesteps)
             x = block(x)
         return x
 
+    def delta_sigma_current(self, t: int) -> Tensor:
+        if self.delta_sigma_min is None: return self.delta_sigma_max
+        return torch.tensor(bounded_sigmoid(t, **self.delta_sigma_params))
+
     def loss(
         self,
-        x: Tensor,
-        x_hat: Tensor,
-        z_q: Tensor,
-        z_e: Tensor,
-        z_e_chunk: Tensor,
-        encoding_idx: Tensor,
-        perplexity: Tensor,
+        x_i: Tensor,
+        x_i_framed: Tensor,
+        x_j: Tensor,
+        x_hat_i: Tensor,
+        x_hat_j: Tensor,
+        x_hat_i_framed: Tensor,
+        z_q_i: Tensor,
+        z_q_j: Tensor,
+        z_e_i: Tensor,
+        z_e_j: Tensor,
+        z_e_i_chunk: Tensor,
+        z_e_j_chunk: Tensor,
+        delta_i: Tensor,
+        delta_j: Tensor,
+        encoding_idx_i: Tensor,
+        encoding_idx_j: Tensor,
+        perplexity_i: Tensor,
+        perplexity_j: Tensor,
         **kwargs: Any
     ) -> Dict[str, Tensor]:
         outputs = dict()
         losses = []
-        # if passed a sequence, ensure invariance to sequence length by treating frames independently
-        if x.size(-2) > self.frame_window_length:
-            x = frame(x, **self.frame_params)
-            x_hat = frame(x_hat, **self.frame_params)
-        # frame-wise reconstruction loss, sum over pixels, average over frames & samples
+        # maximise likelihood p(x_i|z_j) framewise
+        x = torch.cat([x_i_framed, x_j], dim=0)
+        x_hat = torch.cat([x_hat_i_framed, x_hat_j], dim=0)
         log_sigma_sq_z = torch.tensor(self.sigma_z).pow(2).log()
         nll = negative_log_likelihood(x, x_hat, log_sigma_sq_z).flatten(start_dim=-3).sum(dim=-1)
         losses.append(nll.mean())
         mae_frame = (x_hat - x).flatten(start_dim=-3).abs().sum(dim=-1)
         outputs |= dict(log_likelihood_x=-nll.detach().mean(), sigma_z=(0.5 * log_sigma_sq_z).exp().detach(), mae_frame=mae_frame.detach().mean())
+        # MAP estimate of the alignment factor p(x|δ)p(δ) with a gaussian prior on a circular co-ordinate space
+        dt = torch.cat([delta_i.flatten(end_dim=1).unsqueeze(1), delta_j], dim=0)
+        mu_delta_wrt_x = torch.zeros(1).to(dt.device)
+        log_sigma_sq_delta_wrt_x= self.delta_sigma_current(self.trainer.global_step).pow(2).log()
+        intra_frame_nll = negative_log_likelihood(dt, mu_delta_wrt_x, log_sigma_sq_delta_wrt_x)
+        losses.append(intra_frame_nll.mean())
+        outputs |= dict(log_likelihood_dt=-intra_frame_nll.detach().mean())
         # regularise to encourage the encoder to commit to an embedding
+        z_e = torch.cat([z_e_i, z_e_j], dim=0)
+        z_q = torch.cat([z_q_i, z_q_j], dim=0)
         cml = (z_e - z_q.detach()).pow(2).sum(dim=-1)
         cml = self.beta * cml.mean()
         losses.append(cml)
@@ -346,7 +400,7 @@ class EAR(L.LightningModule):
         # outputs |= dict(info_nce=ince.detach())
         # sum the loss components
         outputs |= dict(loss=sum(losses))
-        outputs |= dict(perplexity=perplexity)
+        outputs |= dict(perplexity=torch.cat([perplexity_i, perplexity_j]).mean())
         return outputs
 
     def training_step(self, batch: Tuple[Tensor, Tensor, Tensor], batch_idx: int, **kwargs: Any) -> Dict[str, Tensor]:
@@ -388,24 +442,24 @@ class EAR(L.LightningModule):
             plt.close(fig)
         self.validation_step_outputs.clear()
 
-    @torch.no_grad()
-    def predict_step(self, batch: Tuple[Tensor, Tensor, Tensor], batch_idx: int, dataloader_idx: int = 0, **kwargs: Any) -> None:
-        x, *_ = batch
-        _, x_hat, q_z, *_ = self.predict(x, **kwargs).values()
-        bs, seq, *_ = q_z.size()
-        sample_idx = batch.s.unsqueeze(0).repeat(seq, 1).t().flatten().unsqueeze(1).cpu()
-        seq_idx = torch.arange(seq).repeat(bs, 1).view(bs * seq, 1).cpu()
-        dl_idx = torch.tensor(dataloader_idx).expand(bs).unsqueeze(0).repeat(seq, 1).t().flatten().unsqueeze(1).cpu()
-        mae = (x_hat - x).abs().flatten(start_dim=-3).mean(dim=-1).flatten(end_dim=1).unsqueeze(-1)
-        q_z = q_z.flatten(end_dim=1)
-        data = torch.cat([sample_idx.cpu(), seq_idx.cpu(), dl_idx.cpu(), q_z.cpu(), mae.cpu()], dim=-1)
-        index_columns  = ["file_i", "timestep", "dataloader_idx"]
-        z_mean_cols, z_log_var_cols = [f"z_mean_{d}" for d in range(q_z.size(-1)//2)], [f"z_log_var_{d}" for d in range(q_z.size(-1)//2)]
-        data_columns = [z_mean_cols, z_log_var_cols, ["mae"]]
-        dtypes = dict(**dict([(col, int) for col in index_columns]), **dict([(col, float) for columns in data_columns for col in columns]))
-        df = pd.DataFrame(data=data, columns=[*index_columns, *sum(data_columns, [])]).astype(dtype=dtypes).set_index(index_columns)
-        self.predict_step_outputs.append(df)
-        return df
+    # @torch.no_grad()
+    # def predict_step(self, batch: Tuple[Tensor, Tensor, Tensor], batch_idx: int, dataloader_idx: int = 0, **kwargs: Any) -> None:
+    #     x, *_ = batch
+    #     _, x_hat, q_z, *_ = self.predict(x, **kwargs).values()
+    #     bs, seq, *_ = q_z.size()
+    #     sample_idx = batch.s.unsqueeze(0).repeat(seq, 1).t().flatten().unsqueeze(1).cpu()
+    #     seq_idx = torch.arange(seq).repeat(bs, 1).view(bs * seq, 1).cpu()
+    #     dl_idx = torch.tensor(dataloader_idx).expand(bs).unsqueeze(0).repeat(seq, 1).t().flatten().unsqueeze(1).cpu()
+    #     mae = (x_hat - x).abs().flatten(start_dim=-3).mean(dim=-1).flatten(end_dim=1).unsqueeze(-1)
+    #     q_z = q_z.flatten(end_dim=1)
+    #     data = torch.cat([sample_idx.cpu(), seq_idx.cpu(), dl_idx.cpu(), q_z.cpu(), mae.cpu()], dim=-1)
+    #     index_columns  = ["file_i", "timestep", "dataloader_idx"]
+    #     z_mean_cols, z_log_var_cols = [f"z_mean_{d}" for d in range(q_z.size(-1)//2)], [f"z_log_var_{d}" for d in range(q_z.size(-1)//2)]
+    #     data_columns = [z_mean_cols, z_log_var_cols, ["mae"]]
+    #     dtypes = dict(**dict([(col, int) for col in index_columns]), **dict([(col, float) for columns in data_columns for col in columns]))
+    #     df = pd.DataFrame(data=data, columns=[*index_columns, *sum(data_columns, [])]).astype(dtype=dtypes).set_index(index_columns)
+    #     self.predict_step_outputs.append(df)
+    #     return df
 
     def configure_optimizers(self) -> Optimizer:
         optimiser_config = DictConfig(dict(_target_=self.optimiser_cls, **(self.optimiser_config or {})))
