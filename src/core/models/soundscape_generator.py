3d8b0c47f20b371791b18ec501c3ec9564bcd4d8
Kieran Gibb <kg@magmacollective.org>
2026-01-25 18:50:53 +0000

    code for first pass at hierarchical AR model

--- src/core/models/soundscape_generator.py
+++ src/core/models/soundscape_generator.py
@@ -21,7 +21,9 @@ __all__ = ["SoundscapeGenerator"]
 
 @dataclass(unsafe_hash=True, kw_only=True)
 class SoundscapeGenerator(L.LightningModule):
-    num_classes: 512
+    chunk_size: int = 8
+    num_chunks: int = 16
+    num_classes: int = 512
     embedding_dim: int = 64
     attn_num_enc_heads: int = 2
     attn_num_dec_heads: int = 2
@@ -54,25 +56,57 @@ class SoundscapeGenerator(L.LightningModule):
         # map vqvae codebook indices to embeddings
         self.embedding = torch.nn.Embedding(self.num_classes, self.embedding_dim)
         # learn a token that initializes the auto-regressive process
-        self.init_token = torch.nn.Parameter(torch.zeros(1, 1, self.embedding_dim))
-        # use encoded attentions to decode sequence autoregressively
-        self.sequence_decoder = TransformerEncoder(
+        self.init_token = torch.nn.Parameter(torch.zeros(1, 1, self.chunk_size * self.num_chunks))
+        # intra-frame chunk attention
+        self.chunk_encoder = TransformerEncoder(
             input_size=self.embedding_dim,
             mlp_ratio=self.attn_mlp_ratio,
             depth=self.attn_dec_depth,
             num_heads=self.attn_num_dec_heads,
         )
-        self.projection = torch.nn.Linear(in_features=self.embedding_dim, out_features=self.num_classes)
+        # attention pooling for sequence-level summary
+        self.query = torch.nn.Parameter(torch.randn(self.embedding_dim))
+        torch.nn.init.normal_(self.query, std=0.02)
+        self.proj = torch.nn.Linear(self.embedding_dim, self.chunk_size * self.num_chunks)
+        # inter-frame auto-regressive model
+        self.sequence_encoder = TransformerEncoder(
+            input_size=self.chunk_size * self.num_chunks,
+            mlp_ratio=self.attn_mlp_ratio,
+            depth=self.attn_dec_depth,
+            num_heads=self.attn_num_dec_heads,
+        )
+        # condition chunks on AR frame-wise information
+        self.chunk_conditioner = torch.nn.Linear(self.chunk_size * self.num_chunks, self.embedding_dim)
+        # predict chunk categories for each frame
+        self.chunk_predictor = torch.nn.Linear(in_features=self.embedding_dim, out_features=self.num_classes)
         self._reset_cache()
 
     def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> Dict[str, torch.Tensor]:
-        x = x.flatten(start_dim=1)
-        x_e = self.embedding(x)
-        x_e = x_e + self.positional_encoding(x_e.size(1), x_e.size(2)).to(x_e.device)
-        attn_mask = self.causal_mask(x_e.size(1)).to(x_e.device)
-        x_in = torch.cat([self.init_token.expand(x_e.size(0), 1, -1), x_e[:, :-1]], dim=1)
-        x_out, _, attn_w = self.sequence_decoder(x_in, attn_mask=attn_mask)
-        logits = self.projection(x_out)
+        # drop the last timestep and concatenate on zeros at the beginning to force future prediction
+        # extract dims for easy use
+        bs, seq, ch = x.size()
+        # embed chunks
+        x_e = self.embedding(x[:, :-1]) # (bs, seq, ch, em)
+        x_e = x_e.flatten(end_dim=1)
+        # intra-frame attention across chunks
+        h, *_ = self.chunk_encoder(x_e)
+        h = h.unflatten(0, (bs, seq - 1))
+        # attention pooling
+        scores = (h @ self.query) / (h.size(-1) ** 0.5)
+        w = torch.softmax(scores, dim=2)
+        h_pool = (h * w.unsqueeze(-1)).sum(dim=2) # (bs, seq, em)
+        z = self.proj(h_pool) # (bs, seq, ld)
+        # stack init token and apply positional encoding
+        z = torch.cat([self.init_token.expand(z.size(0), 1, -1), z], dim=1)
+        z = z + self.positional_encoding(z.size(1), z.size(2)).to(z.device)
+        attn_mask = self.causal_mask(z.size(1)).to(z.device)
+        # inter-frame auto-regression for frame-wise contextual summary across sequence
+        z_hat, _, attn_w = self.sequence_encoder(z, attn_mask=attn_mask)
+        context = self.chunk_conditioner(z_hat).unsqueeze(2)
+        # predictor uses chunks at t and sequence context to predict next logits
+        h0 = torch.zeros(h.size(0), 1, h.size(2), h.size(3), dtype=torch.float32).to(h.device)
+        h = torch.cat([h0, h], dim=1)
+        logits = self.chunk_predictor(h + context)
         return dict(x=x, logits=logits, attn_w=attn_w)
 
     @staticmethod
@@ -90,8 +124,8 @@ class SoundscapeGenerator(L.LightningModule):
         return torch.triu(neg_inf, diagonal=1).float()
 
     def loss(self, x: torch.Tensor, logits: torch.Tensor, **kwargs: Any) -> Dict[str, torch.Tensor]:
-        cel = F.cross_entropy(logits.flatten(end_dim=1), x.flatten(end_dim=1), reduction="none").unflatten(0, (x.size(0), x.size(1)))
-        # loss scale???
+        cel = F.cross_entropy(logits.view(-1, logits.size(-1)), x.view(-1), reduction="none")
+        cel = cel.view(x.size(0), x.size(1), x.size(2))
         cel = cel.sum(dim=1).mean()
         return dict(loss=cel, cel=cel.detach())
 
