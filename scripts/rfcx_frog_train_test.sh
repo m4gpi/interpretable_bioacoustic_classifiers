60fe9f73f67460cacce52af1156156094cb8ca1c
Kieran Gibb <kg@magmacollective.org>
2026-04-13 13:54:26 +0100

    correct eval scripts

--- scripts/rfcx_frog_train_test.sh
+++ scripts/rfcx_frog_train_test.sh
@@ -27,10 +27,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
    model.clf_learning_rate=3.e-2 \
+   model.l1_penalty=1.e-2 \
+   model.attn_learning_rate=5.e-4 \
+   model.attn_weight_decay=1.e-3 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=rfcx_frog_base_vae.pt:v7 \
@@ -51,10 +51,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
+   model.clf_learning_rate=3.e-2 \
+   model.l1_penalty=1.e-2 \
    model.attn_weight_decay=1.e-3 \
    model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=3.e-2 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=rfcx_frog_base_vae.pt:v8 \
@@ -75,10 +75,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
+   model.clf_learning_rate=3.e-2 \
+   model.l1_penalty=1.e-2 \
    model.attn_weight_decay=1.e-3 \
    model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=3.e-2 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=rfcx_frog_base_vae.pt:v9 \
@@ -111,10 +111,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
+   model.clf_learning_rate=1.e-1 \
+   model.l1_penalty=1.e-2 \
    model.attn_weight_decay=1.e-3 \
    model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=rfcx_frog_nifti_vae.pt:v14 \
@@ -135,10 +135,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
+   model.clf_learning_rate=1.e-1 \
+   model.l1_penalty=1.e-2 \
    model.attn_weight_decay=1.e-3 \
    model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=rfcx_frog_nifti_vae.pt:v15 \
@@ -159,10 +159,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
+   model.clf_learning_rate=1.e-1 \
+   model.l1_penalty=1.e-2 \
    model.attn_weight_decay=1.e-3 \
    model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=rfcx_frog_nifti_vae.pt:v16 \
@@ -170,89 +170,89 @@ uv run main.py train \
    callbacks.model_checkpoint.save_last=null \
    paths.results_dir=$RESULTS_DIR
 
-# model                 smooth_nifti_vae
-# scope                        RFCX_frog
-# pool_method                  prob_attn
-# clf_learning_rate                  0.1
-# l1_penalty                        0.01
-# attn_learning_rate               0.001
-# attn_weight_decay                0.001
-# epoch                              499
-# auROC                         0.901212
-# AP                            0.291005
-# score                         1.192217
+# # model                 smooth_nifti_vae
+# # scope                        RFCX_frog
+# # pool_method                  prob_attn
+# # clf_learning_rate                  0.1
+# # l1_penalty                        0.01
+# # attn_learning_rate               0.001
+# # attn_weight_decay                0.001
+# # epoch                              499
+# # auROC                         0.901212
+# # AP                            0.291005
+# # score                         1.192217
 
-uv run main.py train \
-   +experiment=species_detector \
-   project=species_detector \
-   'trainer.devices=[3]' \
-   trainer.max_epochs=500 \
-   +trainer.limit_val_batches=0.0 \
-   data.model=smooth_nifti_vae \
-   data.version=v3 \
-   data.scope=RFCX_frog \
-   data.val_prop=0.0 \
-   model.pool_method=prob_attn \
-   model.eval_sample_size=100 \
-   model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=1.e-3 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
-   model.key_per_target=true \
-   model.penalty_multiplier=2 \
-   callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
-   callbacks.model_checkpoint.filename=rfcx_frog_smooth_nifti_vae.pt:v3 \
-   callbacks.model_checkpoint.monitor=null \
-   callbacks.model_checkpoint.save_last=null \
-   paths.results_dir=$RESULTS_DIR
+# uv run main.py train \
+#    +experiment=species_detector \
+#    project=species_detector \
+#    'trainer.devices=[3]' \
+#    trainer.max_epochs=500 \
+#    +trainer.limit_val_batches=0.0 \
+#    data.model=smooth_nifti_vae \
+#    data.version=v3 \
+#    data.scope=RFCX_frog \
+#    data.val_prop=0.0 \
+#    model.pool_method=prob_attn \
+#    model.eval_sample_size=100 \
+#    model.attn_dim=10 \
+#    model.clf_learning_rate=1.e-1 \
+#    model.l1_penalty=1.e-2 \
+#    model.attn_weight_decay=1.e-3 \
+#    model.attn_learning_rate=1.e-3 \
+#    model.key_per_target=true \
+#    model.penalty_multiplier=2 \
+#    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
+#    callbacks.model_checkpoint.filename=rfcx_frog_smooth_nifti_vae.pt:v3 \
+#    callbacks.model_checkpoint.monitor=null \
+#    callbacks.model_checkpoint.save_last=null \
+#    paths.results_dir=$RESULTS_DIR
 
-uv run main.py train \
-   +experiment=species_detector \
-   project=species_detector \
-   'trainer.devices=[3]' \
-   trainer.max_epochs=500 \
-   +trainer.limit_val_batches=0.0 \
-   data.model=smooth_nifti_vae \
-   data.version=v4 \
-   data.scope=RFCX_frog \
-   data.val_prop=0.0 \
-   model.pool_method=prob_attn \
-   model.eval_sample_size=100 \
-   model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=1.e-3 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
-   model.key_per_target=true \
-   model.penalty_multiplier=2 \
-   callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
-   callbacks.model_checkpoint.filename=rfcx_frog_smooth_nifti_vae.pt:v4 \
-   callbacks.model_checkpoint.monitor=null \
-   callbacks.model_checkpoint.save_last=null \
-   paths.results_dir=$RESULTS_DIR
+# uv run main.py train \
+#    +experiment=species_detector \
+#    project=species_detector \
+#    'trainer.devices=[3]' \
+#    trainer.max_epochs=500 \
+#    +trainer.limit_val_batches=0.0 \
+#    data.model=smooth_nifti_vae \
+#    data.version=v4 \
+#    data.scope=RFCX_frog \
+#    data.val_prop=0.0 \
+#    model.pool_method=prob_attn \
+#    model.eval_sample_size=100 \
+#    model.attn_dim=10 \
+#    model.clf_learning_rate=1.e-1 \
+#    model.l1_penalty=1.e-2 \
+#    model.attn_weight_decay=1.e-3 \
+#    model.attn_learning_rate=1.e-3 \
+#    model.key_per_target=true \
+#    model.penalty_multiplier=2 \
+#    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
+#    callbacks.model_checkpoint.filename=rfcx_frog_smooth_nifti_vae.pt:v4 \
+#    callbacks.model_checkpoint.monitor=null \
+#    callbacks.model_checkpoint.save_last=null \
+#    paths.results_dir=$RESULTS_DIR
 
-uv run main.py train \
-   +experiment=species_detector \
-   project=species_detector \
-   'trainer.devices=[3]' \
-   trainer.max_epochs=500 \
-   +trainer.limit_val_batches=0.0 \
-   data.model=smooth_nifti_vae \
-   data.version=v5 \
-   data.scope=RFCX_frog \
-   data.val_prop=0.0 \
-   model.pool_method=prob_attn \
-   model.eval_sample_size=100 \
-   model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=1.e-3 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
-   model.key_per_target=true \
-   model.penalty_multiplier=2 \
-   callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
-   callbacks.model_checkpoint.filename=rfcx_frog_smooth_nifti_vae.pt:v5 \
-   callbacks.model_checkpoint.monitor=null \
-   callbacks.model_checkpoint.save_last=null \
-   paths.results_dir=$RESULTS_DIR
+# uv run main.py train \
+#    +experiment=species_detector \
+#    project=species_detector \
+#    'trainer.devices=[3]' \
+#    trainer.max_epochs=500 \
+#    +trainer.limit_val_batches=0.0 \
+#    data.model=smooth_nifti_vae \
+#    data.version=v5 \
+#    data.scope=RFCX_frog \
+#    data.val_prop=0.0 \
+#    model.pool_method=prob_attn \
+#    model.eval_sample_size=100 \
+#    model.attn_dim=10 \
+#    model.clf_learning_rate=1.e-1 \
+#    model.l1_penalty=1.e-2 \
+#    model.attn_weight_decay=1.e-3 \
+#    model.attn_learning_rate=1.e-3 \
+#    model.key_per_target=true \
+#    model.penalty_multiplier=2 \
+#    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
+#    callbacks.model_checkpoint.filename=rfcx_frog_smooth_nifti_vae.pt:v5 \
+#    callbacks.model_checkpoint.monitor=null \
+#    callbacks.model_checkpoint.save_last=null \
+#    paths.results_dir=$RESULTS_DIR
--- scripts/so_ec_train_test.sh
+++ scripts/so_ec_train_test.sh
@@ -34,10 +34,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
    model.clf_learning_rate=3.e-2 \
+   model.l1_penalty=1.e-2 \
+   model.attn_learning_rate=5.e-4 \
+   model.attn_weight_decay=1.e-3 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=so_ec_base_vae.pt:v4 \
@@ -58,10 +58,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
    model.clf_learning_rate=3.e-2 \
+   model.l1_penalty=1.e-2 \
+   model.attn_learning_rate=5.e-4 \
+   model.attn_weight_decay=1.e-3 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=so_ec_base_vae.pt:v5 \
@@ -82,10 +82,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=5.e-4 \
-   model.l1_penalty=1.e-2 \
    model.clf_learning_rate=3.e-2 \
+   model.l1_penalty=1.e-2 \
+   model.attn_learning_rate=5.e-4 \
+   model.attn_weight_decay=1.e-3 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=so_ec_base_vae.pt:v6 \
@@ -118,10 +118,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
-   model.attn_weight_decay=1.e-2 \
-   model.attn_learning_rate=5.e-4 \
+   model.clf_learning_rate=3.e-2 \
    model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-2 \
+   model.attn_learning_rate=5.e-4 \
+   model.attn_weight_decay=1.e-2 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=so_ec_nifti_vae.pt:v12 \
@@ -142,10 +142,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
-   model.attn_weight_decay=1.e-2 \
-   model.attn_learning_rate=5.e-4 \
+   model.clf_learning_rate=3.e-2 \
    model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-2 \
+   model.attn_learning_rate=5.e-4 \
+   model.attn_weight_decay=1.e-2 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=so_ec_nifti_vae.pt:v17 \
@@ -166,10 +166,10 @@ uv run main.py train \
    model.pool_method=prob_attn \
    model.eval_sample_size=100 \
    model.attn_dim=10 \
-   model.attn_weight_decay=1.e-2 \
-   model.attn_learning_rate=5.e-4 \
+   model.clf_learning_rate=3.e-2 \
    model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-2 \
+   model.attn_learning_rate=5.e-4 \
+   model.attn_weight_decay=1.e-2 \
    model.key_per_target=true \
    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
    callbacks.model_checkpoint.filename=so_ec_nifti_vae.pt:v18 \
@@ -177,89 +177,89 @@ uv run main.py train \
    callbacks.model_checkpoint.save_last=null \
    paths.results_dir=$RESULTS_DIR
 
-# model                 smooth_nifti_vae
-# scope                            SO_EC
-# pool_method                  prob_attn
-# clf_learning_rate                  0.1
-# l1_penalty                        0.01
-# attn_learning_rate               0.001
-# attn_weight_decay                0.001
-# epoch                             1949
-# auROC                         0.871199
-# AP                            0.324339
-# score                         1.195538
+# # model                 smooth_nifti_vae
+# # scope                            SO_EC
+# # pool_method                  prob_attn
+# # clf_learning_rate                  0.1
+# # l1_penalty                        0.01
+# # attn_learning_rate               0.001
+# # attn_weight_decay                0.001
+# # epoch                             1949
+# # auROC                         0.871199
+# # AP                            0.324339
+# # score                         1.195538
 
-uv run main.py train \
-   +experiment=species_detector \
-   project=species_detector \
-   'trainer.devices=[0]' \
-   trainer.max_epochs=1950 \
-   +trainer.limit_val_batches=0.0 \
-   data.model=smooth_nifti_vae \
-   data.version=v0 \
-   data.scope=SO_EC \
-   data.val_prop=0.0 \
-   model.pool_method=prob_attn \
-   model.eval_sample_size=100 \
-   model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=1.e-3 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
-   model.key_per_target=true \
-   model.penalty_multiplier=2 \
-   callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
-   callbacks.model_checkpoint.filename=so_ec_smooth_nifti_vae.pt:v0 \
-   callbacks.model_checkpoint.monitor=null \
-   callbacks.model_checkpoint.save_last=null \
-   paths.results_dir=$RESULTS_DIR
+# uv run main.py train \
+#    +experiment=species_detector \
+#    project=species_detector \
+#    'trainer.devices=[0]' \
+#    trainer.max_epochs=1950 \
+#    +trainer.limit_val_batches=0.0 \
+#    data.model=smooth_nifti_vae \
+#    data.version=v0 \
+#    data.scope=SO_EC \
+#    data.val_prop=0.0 \
+#    model.pool_method=prob_attn \
+#    model.eval_sample_size=100 \
+#    model.attn_dim=10 \
+#    model.attn_weight_decay=1.e-3 \
+#    model.attn_learning_rate=1.e-3 \
+#    model.l1_penalty=1.e-2 \
+#    model.clf_learning_rate=1.e-1 \
+#    model.key_per_target=true \
+#    model.penalty_multiplier=2 \
+#    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
+#    callbacks.model_checkpoint.filename=so_ec_smooth_nifti_vae.pt:v0 \
+#    callbacks.model_checkpoint.monitor=null \
+#    callbacks.model_checkpoint.save_last=null \
+#    paths.results_dir=$RESULTS_DIR
 
-uv run main.py train \
-   +experiment=species_detector \
-   project=species_detector \
-   'trainer.devices=[0]' \
-   trainer.max_epochs=1950 \
-   +trainer.limit_val_batches=0.0 \
-   data.model=smooth_nifti_vae \
-   data.version=v1 \
-   data.scope=SO_EC \
-   data.val_prop=0.0 \
-   model.pool_method=prob_attn \
-   model.eval_sample_size=100 \
-   model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=1.e-3 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
-   model.key_per_target=true \
-   model.penalty_multiplier=2 \
-   callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
-   callbacks.model_checkpoint.filename=so_ec_smooth_nifti_vae.pt:v1 \
-   callbacks.model_checkpoint.monitor=null \
-   callbacks.model_checkpoint.save_last=null \
-   paths.results_dir=$RESULTS_DIR
+# uv run main.py train \
+#    +experiment=species_detector \
+#    project=species_detector \
+#    'trainer.devices=[0]' \
+#    trainer.max_epochs=1950 \
+#    +trainer.limit_val_batches=0.0 \
+#    data.model=smooth_nifti_vae \
+#    data.version=v1 \
+#    data.scope=SO_EC \
+#    data.val_prop=0.0 \
+#    model.pool_method=prob_attn \
+#    model.eval_sample_size=100 \
+#    model.attn_dim=10 \
+#    model.attn_weight_decay=1.e-3 \
+#    model.attn_learning_rate=1.e-3 \
+#    model.l1_penalty=1.e-2 \
+#    model.clf_learning_rate=1.e-1 \
+#    model.key_per_target=true \
+#    model.penalty_multiplier=2 \
+#    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
+#    callbacks.model_checkpoint.filename=so_ec_smooth_nifti_vae.pt:v1 \
+#    callbacks.model_checkpoint.monitor=null \
+#    callbacks.model_checkpoint.save_last=null \
+#    paths.results_dir=$RESULTS_DIR
 
-uv run main.py train \
-   +experiment=species_detector \
-   project=species_detector \
-   'trainer.devices=[0]' \
-   trainer.max_epochs=1950 \
-   +trainer.limit_val_batches=0.0 \
-   data.model=smooth_nifti_vae \
-   data.version=v2 \
-   data.scope=SO_EC \
-   data.val_prop=0.0 \
-   model.pool_method=prob_attn \
-   model.eval_sample_size=100 \
-   model.attn_dim=10 \
-   model.attn_weight_decay=1.e-3 \
-   model.attn_learning_rate=1.e-3 \
-   model.l1_penalty=1.e-2 \
-   model.clf_learning_rate=1.e-1 \
-   model.key_per_target=true \
-   model.penalty_multiplier=2 \
-   callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
-   callbacks.model_checkpoint.filename=so_ec_smooth_nifti_vae.pt:v2 \
-   callbacks.model_checkpoint.monitor=null \
-   callbacks.model_checkpoint.save_last=null \
-   paths.results_dir=$RESULTS_DIR
+# uv run main.py train \
+#    +experiment=species_detector \
+#    project=species_detector \
+#    'trainer.devices=[0]' \
+#    trainer.max_epochs=1950 \
+#    +trainer.limit_val_batches=0.0 \
+#    data.model=smooth_nifti_vae \
+#    data.version=v2 \
+#    data.scope=SO_EC \
+#    data.val_prop=0.0 \
+#    model.pool_method=prob_attn \
+#    model.eval_sample_size=100 \
+#    model.attn_dim=10 \
+#    model.attn_weight_decay=1.e-3 \
+#    model.attn_learning_rate=1.e-3 \
+#    model.l1_penalty=1.e-2 \
+#    model.clf_learning_rate=1.e-1 \
+#    model.key_per_target=true \
+#    model.penalty_multiplier=2 \
+#    callbacks.model_checkpoint.dirpath=$RESULTS_DIR/checkpoints \
+#    callbacks.model_checkpoint.filename=so_ec_smooth_nifti_vae.pt:v2 \
+#    callbacks.model_checkpoint.monitor=null \
+#    callbacks.model_checkpoint.save_last=null \
+#    paths.results_dir=$RESULTS_DIR
