c14972df580974d87403bdce864e915a8f1e079a
Kieran Gibb <kg@magmacollective.org>
2026-04-20 13:36:55 +0100

    load hparams from checkpoint

--- src/cli/eval.py
+++ src/cli/eval.py
@@ -5,6 +5,7 @@ import logging
 import os
 import pathlib
 import rootutils
+import torch
 import warnings
 import wandb
 
@@ -28,8 +29,12 @@ def evaluate(cfg):
 
     log.info(f"Instantiating model <{cfg.model._target_}>")
     model_cls = hydra.utils.get_class(cfg.model._target_)
-    filtered_params = filter_kwargs_for_callable(model_cls.__init__, data_module.data.model_params)
-    model = hydra.utils.instantiate(cfg.model, **filtered_params)
+
+    if (ckpt_path := cfg.get("ckpt_path")):
+        model = model_cls.load_from_checkpoint(ckpt_path, map_location=torch.device("cuda"))
+    else:
+        filtered_params = filter_kwargs_for_callable(model_cls.__init__, data_module.data.model_params)
+        model = hydra.utils.instantiate(cfg.model, **filtered_params)
 
     log.info("Instantiating callbacks...")
     callbacks: List[L.Callback] = instantiate_callbacks(cfg.get("callbacks"))
